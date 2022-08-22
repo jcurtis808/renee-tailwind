64dbda8e5f7112c29f848606b4aee83de186fae8
jcurtis808 <jeremy.curtis808@gmail.com>
2022-08-21 21:22:26 -0500

    adding fade-in effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        "fade-in": "fadeIn 0.5s ease-in-out"
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      }),
       backgroundImage: (theme) => ({
         landing: "url('./images/landing-mobile.jpg')",
       }),
